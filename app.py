5e9e4f5ef0137d571259b7375e011d4caafd842c
edgzoah <52865203+edgzoah@users.noreply.github.com>
2022-03-23 15:06:16 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,37 @@
-from flask import Flask , request , abort , redirect , Response ,url_for, render_template, flash
+from django.shortcuts import redirect
+from flask import Flask , request , render_template , send_file
 import os
 from werkzeug.utils import secure_filename
+from datetime import datetime
+import fitz
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'F3HUIF23H8923F9H8389FHXKLN'
 app.config['UPLOAD_FOLDER'] = './templates'
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/sent', methods=['POST', 'GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def sent():
     if request.method == 'POST':
+        if request.form['page']:
+            print(request.form['page'])
         uploaded_files = request.files.getlist('file')
-        print(uploaded_files)
+        doc1 = fitz.open()
+        now1 = datetime.now()
+        d1 = now1.strftime("%d.%m.%Y")
+        current_time = now1.strftime("%H:%M:%S")
+        fajnie = str(d1) + ':' + str(current_time)
         for i in uploaded_files:
-            i.save(secure_filename(i.filename))
+            f = secure_filename(i.filename)
+            i.save(f)
+            doc = fitz.open(f)
+            # if request.form['page']: doc1.insert_pdf(doc, from_page = doc.page_count)
+            # else: doc1.insert_pdf(doc, to_page = 0)
+            doc1.insert_pdf(doc, to_page = 0)
+            doc1.save(fajnie + ".pdf")
+            os.remove('/home/adam/pdf/pdfupload/' + f)
+        return send_file('./' + fajnie + '.pdf', as_attachment=True)
     return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
