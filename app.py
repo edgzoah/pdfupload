3c0e9a2d5f0423004c21a556600f916459a9b252
edgzoah <52865203+edgzoah@users.noreply.github.com>
2022-03-26 00:50:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ app.config['SECRET_KEY'] = 'F3HUIF23H8923F9H8389FHXKLN'
 def sent():
     if request.method == 'POST':
         uploaded_files = request.files.getlist('PDFs[]')
-        print(uploaded_files) # WRZUCONE PLIKI
-        print(request.headers['fajne']) # TO JEST TEN SWITCH
         doc1 = fitz.open()
         now1 = datetime.now()
         d1 = now1.strftime("%d%m%Y")
@@ -24,11 +22,8 @@ def sent():
             f = secure_filename(i.filename)
             i.save(f)
             doc = fitz.open(f)
-            if request.headers['fajne'] == True: print('gogol') # JESLI ZAZNACZONY TO PRINT GOGOL 
-            else: print('niegogol') # JESLI NIE TO PRINT NIEGOGOL
-            # if request.headers['fajne'] == True: doc1.insert_pdf(doc, from_page = doc.page_count)
-            # else: doc1.insert_pdf(doc, to_page = 0)
-            doc1.insert_pdf(doc, to_page=0)
+            if request.headers['fajne'] == 'true': doc1.insert_pdf(doc, from_page = doc.page_count)
+            else: doc1.insert_pdf(doc, to_page = 0)
             doc1.save(file_name + ".pdf")
             doc.close()
             os.remove(f)
